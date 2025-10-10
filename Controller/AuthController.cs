7e2451384dd7787a113d93f77276b730f32b6f4a
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-10 07:37:41 -0600

    Añadir controlador de autenticación y solicitudes DTO

---
+++ Controller/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PruebaBackend.DTOs;
+using PruebaBackend.Services;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly AuthService _authService;
+
+    public AuthController(AuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] AuthRequests.Login request)
+    {
+        var result = await _authService.Login(request);
+        if (result != null)
+        {
+            return Ok(result);
+        }
+        return Unauthorized();
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] AuthRequests.Register request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _authService.Register(request);
+
+        if (result is IdentityResult identityResult && !identityResult.Succeeded)
+        {
+            var errorMsg = identityResult.Errors.FirstOrDefault()?.Description ?? "¡La creación del usuario falló! Por favor, revisa los detalles del usuario y vuelve a intentarlo.";
+            return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = errorMsg });
+        }
+
+        return Ok(result);
+    }
+}
+
---
+++ DTOs/AuthRequests.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaBackend.DTOs
+{
+    public class AuthRequests
+    {
+        public record Login(
+            [Required] string email,
+            [Required] string Password
+            );
+
+        public record Register(
+            [Required] string Nombre,
+            [Required] string Apellidos,
+            [Required] string Username,
+            [Required] string Email,
+            [Required] string Password
+            );
+    }
+}
