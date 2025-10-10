77d7817323a21fbb39a9d216a0a9d9c865e84ccb
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-10 07:35:28 -0600

    Integración de ASP.NET Identity en ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PruebaBackend.Models;
 
 namespace PruebaBackend.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<Usuario, IdentityRole<int>, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -11,14 +14,20 @@ namespace PruebaBackend.Data
 
         public DbSet<Permiso> Permisos { get; set; }
         public DbSet<TipoPermiso> TipoPermisos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<TipoPermiso>().HasData(
                 new TipoPermiso { Id = 1, Descripcion = "Enfermedad" },
                 new TipoPermiso { Id = 2, Descripcion = "Diligencias" },
                 new TipoPermiso { Id = 3, Descripcion = "Otros" }
             );
+
+            
         }
     }
 }
