31e547a66417dfd2e54128b396876de0dd4f98a9
ricardo.reyes forteinnovation.net <ricardo.reyes@forteinnovation.net>
2025-10-15 17:37:38 -0600

    Se agregaron nuevas reglas de negocio en PermisoService

--- Services/PermisoService.cs
+++ Services/PermisoService.cs
@@ -1,21 +1,43 @@
-﻿using PruebaBackend.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using PruebaBackend.DTOs;
+using PruebaBackend.Models;
 using PruebaBackend.Repositories;
+
+
+
 
 namespace PruebaBackend.Services
 {
     public class PermisoService
     {
         private readonly IRepository<Permiso> _permisoRepository;
+        private readonly UserManager<Usuario> _userManager;
 
-        public PermisoService(IRepository<Permiso> permisoRepository)
+
+        public PermisoService(IRepository<Permiso> permisoRepository, UserManager<Usuario> userManager)
         {
             _permisoRepository = permisoRepository;
+            _userManager = userManager;
         }
 
-        public async Task<Permiso> CreateAsync(Permiso permiso)
+        public async Task<Permiso> CreateAsync(PermisoDTOs.NewPermiso newPermiso, int idUsuario)
         {
+            var usuario = _userManager.Users.FirstOrDefault(u => u.Id == idUsuario);
+            if (usuario == null)
+            {
+                throw new ArgumentException("Usuario no encontrado");
+            }
+
+            var permiso = PermisoDTOs.ToModel(newPermiso);
+
+            permiso.UsuarioId = idUsuario;
+            permiso.NombreEmpleado = usuario.Nombre;
+            permiso.ApellidosEmpleado = usuario.Apellidos;
+
             await _permisoRepository.AddAsync(permiso);
             await _permisoRepository.SaveChangesAsync();
+            permiso = await _permisoRepository.GetByIdAsync(permiso.Id); 
+
             return permiso;
         }
 
@@ -35,8 +57,14 @@ namespace PruebaBackend.Services
 
         public async Task<IEnumerable<Permiso>> GetMyAsync(int usuarioId)
         {
-            var allPermisos = await _permisoRepository.GetAllAsync();
-            return allPermisos.Where(p => p.UsuarioId == usuarioId);
+            var myPermisos = await _permisoRepository.FindAsync(p => p.UsuarioId == usuarioId);
+            return myPermisos;
+        }
+
+        public async Task<IEnumerable<Permiso>> GetPendientesAsync()
+        {
+            var pendingPermisos = await _permisoRepository.FindAsync(p => p.IdEstatusPermiso == 1);
+            return pendingPermisos;
         }
 
         public async Task<Permiso> GetByIdAsync(int id)
@@ -44,19 +72,33 @@ namespace PruebaBackend.Services
             return await _permisoRepository.GetByIdAsync(id);
         }
 
-        public async Task<bool> UpdateAsync(Permiso permiso)
+        public async Task<bool> UpdateAsync(PermisoDTOs.PermisoDTO dto)
         {
-            var existingPermiso = await _permisoRepository.GetByIdAsync(permiso.Id);
+            var existingPermiso = await _permisoRepository.GetByIdAsync(dto.Id);
             if (existingPermiso == null) return false;
 
-            existingPermiso.NombreEmpleado = permiso.NombreEmpleado;
-            existingPermiso.ApellidosEmpleado = permiso.ApellidosEmpleado;
-            existingPermiso.TipoPermisoId = permiso.TipoPermisoId;
-            existingPermiso.FechaPermiso = permiso.FechaPermiso;
-
+            existingPermiso = PermisoDTOs.ToModel(dto);
+            
             return await _permisoRepository.SaveChangesAsync();
         }
 
-        
+        internal async Task<bool> ApproveAsync(int id)
+        {
+            var permiso = await _permisoRepository.GetByIdAsync(id);
+            if (permiso == null) return false;
+            permiso.IdEstatusPermiso = 2;
+            permiso.FechaRevision = DateTime.UtcNow;
+            return await _permisoRepository.SaveChangesAsync();
+        }
+
+        internal async Task<bool> DenyAsync(int id, string comment)
+        {
+            var permiso = await _permisoRepository.GetByIdAsync(id);
+            if (permiso == null) return false;
+            permiso.IdEstatusPermiso = 3;
+            permiso.ComentariosSupervisor = comment;
+            permiso.FechaRevision = DateTime.UtcNow;
+            return await _permisoRepository.SaveChangesAsync();
+        }
     }
 }
